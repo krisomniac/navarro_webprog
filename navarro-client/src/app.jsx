28aafcae452a341fd3278641b7417284b57f9dfa
krisomniac <kristinevineonavarro@gmail.com>
2026-05-29 20:10:52 +0800

    fix duplicate import

--- navarro-client/src/app.jsx
+++ navarro-client/src/app.jsx
@@ -13,7 +13,6 @@ import ReportsPage from './pages/DashboardPages/ReportsPage';
 import UsersPage from './pages/DashboardPages/UsersPage';
 import DashLayout from "./components/Layouts/DashLayout";
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import NotFoundPage from './pages/NotFoundPage';
 
 const router = createBrowserRouter([
   {
@@ -21,40 +20,19 @@ const router = createBrowserRouter([
     element: <Layout />,
     errorElement: <NotFoundPage />,
     children: [
-      {
-        index: true,
-        element: <HomePage />
-      },
-      {
-        path: 'about',
-        element: <AboutPage />
-      },
-      {
-        path: 'articles',
-        element: <ArticleListPage />
-      },
-      {
-        path: 'articles/:name',
-        element: <ArticlePage />
-      },
-      {
-        path: '*',
-        element: <NotFoundPage />
-      }
+      { index: true, element: <HomePage /> },
+      { path: 'about', element: <AboutPage /> },
+      { path: 'articles', element: <ArticleListPage /> },
+      { path: 'articles/:name', element: <ArticlePage /> },
+      { path: '*', element: <NotFoundPage /> }
     ]
   },
   {
     path: 'auth',
     element: <AuthLayout />,
     children: [
-      {
-        path: 'signin',
-        element: <SignInPage />
-      },
-      {
-        path: 'signup',
-        element: <SignUpPage />
-      }
+      { path: 'signin', element: <SignInPage /> },
+      { path: 'signup', element: <SignUpPage /> }
     ]
   },
   {
@@ -62,18 +40,9 @@ const router = createBrowserRouter([
     element: <DashLayout />,
     errorElement: <NotFoundPage />,
     children: [
-      {
-        index: true,
-        element: <DashboardPage />
-      },
-      {
-        path: 'reports',
-        element: <ReportsPage />
-      },
-      {
-        path: 'users',
-        element: <UsersPage />
-      }
+      { index: true, element: <DashboardPage /> },
+      { path: 'reports', element: <ReportsPage /> },
+      { path: 'users', element: <UsersPage /> }
     ]
   }
 ]);
